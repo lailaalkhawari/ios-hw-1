3d5b432e37b276032c54375cd7f43a8ba6e5b946
lailaalkhawari <70814104+lailaalkhawari@users.noreply.github.com>
2020-09-11 14:41:37 +0300

    HOMEWORK doneee

--- iOS-Hw1-Part1.playground/Contents.swift
+++ iOS-Hw1-Part1.playground/Contents.swift
@@ -5,7 +5,7 @@
  
  */
 
-let name = "Ahmad"
+var name = "Ahmad"
 
 name = "Salem"
 
@@ -15,11 +15,10 @@ var FriendWeight = 80.5
 var height = 1.5
 
 
-var weightSum = myWeight + FriendWeight
+var weightSum = myWeight + Int(FriendWeight)
 
 
-
-var bmi = myWeight * (height * height)
+var bmi = Double(myWeight) * (height * height)
 
 /*
 - - - -
@@ -38,37 +37,37 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "laila"
+var lastName1 = "alkhawari"
+var email1 = "layla.alk03@gmail.com"
+var phoneNumber1 = "99063641"
+var age1 = 17
+var country1 = "kuwait"
+var area1 = "alshuhada"
+var password = "L2003"
+var isKuwaiti = true
 
 //Person 2
-
-
-
-
-
-
-
-
-
+var firstName2 = "ahmad"
+var lastName2 = "alkhawari"
+var email2 = "ahmad.gmail.com"
+var phoneNumber = 12345678
+var age2 = 23
+var country2 = "USA"
+var area2 = "philly"
+var password2 = "123abc"
+var isKuwaiti2 = false
 
 //Person 3
-
-
-
-
-
-
-
-
-
+var firstName3 = "sara"
+var lastName3 = "alkhawari"
+var email3 = "sara.yahoo.com"
+var phoneNumber3 = 00000000
+var age3 = 25
+var country3 = "italy"
+var area3 = "alshuhada"
+var password3 = "456cde"
+var isKuwaiti3 = true
 /*
 - - - -
 # المطلوب الثالث
@@ -79,6 +78,69 @@ var isKuwaiti = false
  * قارن اذا عمر الاشخاص اصغر من ١٨
  * قارن اذا عمر الاشخاص يساوي  ١٧
  * قارن اذا عمر الاشخاص لا يساوي  ١٧
-
  
  */
+
+ //part1
+
+//مقارنة معلومات بين الاشخاص
+
+(firstName1) == (firstName2)
+(lastName1) == (lastName3)
+
+//بحسب الحروف بالابجدية
+(firstName3) > (firstName2)
+
+(age1) < (age2)
+(age2) == (age3)
+
+ //مقارنة الاعمار
+(age1)==(age2)
+(age1)==(age3)
+(age2)==(age3)
+
+//منطقة السكن
+(area1)==(area2)
+(area1)==(area3)
+(area2)==(area3)
+
+//عمر الـ18
+var E = 18
+
+(age1) > (E)
+(age2) > (E)
+(age3) > (E)
+ 
+(age1) < (E)
+(age2) < (E)
+(age3) < (E)
+
+//عمر الـ17
+var SV = 17
+
+(age1) == (SV)
+(age2) == (SV)
+(age3) == (SV)
+
+(age1) != (SV)
+(age2) != (SV)
+(age3) != (SV)
+
+
+//part2 (ios)
+    
+   * *
+  *   *
+   * *         *****              * *
+             *       *          *     *
+    *       *         *        *      *
+    *       *         *        *
+    *       *         *         *
+    *       *         *          *
+    *       *         *            *
+    *       *         *              *
+    *       *         *       *       *
+    *       *         *       *       *
+    *        *       *         *     *
+    *          *****             * *
+    
